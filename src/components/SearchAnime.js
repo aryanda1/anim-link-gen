442e14e1b32b561f0e483ba9ef719554f65bc97d
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-02-18 17:11:51 +0530

    Used custom hook usehttp to send request and moved pagination logic.

--- src/components/SearchAnime.js
+++ src/components/SearchAnime.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import Paginate from "./Paginate";
 import Titles from "./Titles";
+import useHttp from "../hooks/usehttp";
 
 export default function Form() {
   const ref = useRef();
@@ -8,22 +9,25 @@ export default function Form() {
   const [searched, setSearched] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [titlePerPage, seTtitlePerPage] = useState(5);
+  const { error, isLoading, sendRequest } = useHttp();
+
+  const setDetails = (data) => {
+    const results = data["results"].map((res, idx) => ({ ...res, index: idx }));
+    setAnimeSearchRes(results);
+  };
+
   const submitHandler = async () => {
+    setSearched(true);
     const an_name = ref.current.value;
-    const response = await fetch("http://localhost:5000/search", {
+    const requestConfig = {
+      url: "http://localhost:5000/search",
       method: "POST",
-      body: JSON.stringify({ an_name: an_name }),
+      body: { an_name: an_name },
       headers: {
         "Content-Type": "application/json",
       },
-    });
-    // const data = await response.json();
-    // console.log(data);
-    // const response = await fetch("http://localhost:5000/temp");
-    const data = await response.json();
-    const results = data["results"].map((res, idx) => ({ ...res, index: idx }));
-    setAnimeSearchRes(results);
-    setSearched(true);
+    };
+    sendRequest(requestConfig, setDetails);
   };
 
   const lastTitleIdx = currentPage * titlePerPage;
@@ -37,13 +41,11 @@ export default function Form() {
         <input type="text" ref={ref} id="name" />
       </div>
       <button onClick={submitHandler}>Submit</button>
-      {searched && animeSearchRes.length === 0 && (
-        <p>No anime found! Try Again</p>
-      )}
-      {animeSearchRes.length !== 0 && (
+      {searched && (
         <>
-          <Titles animeDetails={currentDetails} loading={false} />
-          <Paginate
+          <Titles
+            animeDetails={currentDetails}
+            loading={isLoading}
             titlePerPage={titlePerPage}
             totalTitles={animeSearchRes.length}
             paginate={setCurrentPage}
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import styles from "./Titles.module.css";
+import Paginate from "./Paginate";
 
-const Titles = ({ animeDetails, loading }) => {
+const Titles = ({
+  animeDetails,
+  loading,
+  titlePerPage,
+  totalTitles,
+  paginate,
+}) => {
   const [activeId, setActiveId] = useState();
 
   const handleAnimeClick = (e) => {
@@ -9,23 +16,34 @@ const Titles = ({ animeDetails, loading }) => {
   };
   //   console.log(typeof activeId);
 
-  if (loading) return <h2>Loading...</h2>;
-  return (
-    <ul className={styles.list}>
-      {animeDetails.map((detail) => (
-        <li
-          data-index={detail.index}
-          key={detail.index}
-          onClick={handleAnimeClick}
-          className={`${styles.li} ${
-            detail.index === activeId ? styles.active : ""
-          }`}
-        >
-          {detail.name}
-        </li>
-      ))}
-    </ul>
-  );
+  let content = <h2>No anime found! Try Again</h2>;
+  if (animeDetails.length !== 0)
+    content = (
+      <>
+        <ul className={styles.list}>
+          {animeDetails.map((detail) => (
+            <li
+              data-index={detail.index}
+              key={detail.index}
+              onClick={handleAnimeClick}
+              className={`${styles.li} ${
+                detail.index === activeId ? styles.active : ""
+              }`}
+            >
+              {detail.name}
+            </li>
+          ))}
+        </ul>
+        <Paginate
+          titlePerPage={titlePerPage}
+          totalTitles={totalTitles}
+          paginate={paginate}
+        />
+      </>
+    );
+  if (loading) content = <p>Loading...</p>;
+
+  return content;
 };
 
 export default Titles;
