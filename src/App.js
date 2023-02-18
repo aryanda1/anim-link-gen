1b8a3e20bf55f79419f75566a8972afb81682e69
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-02-18 13:47:12 +0530

    Added temporary styles in app.js.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import "./App.css";
 import Form from "./components/Form";
 function App() {
-  return <Form />;
+  return (
+    <div style={{ maxWidth: "40rem", margin: "0 auto" }}>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
