3145d749336de1627225334598658759f2bab5ca
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-02-24 13:46:52 +0530

    Added card wrapper and passed loading and error to title.

--- src/components/ShowSearch.js
+++ src/components/ShowSearch.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Titles from "./Titles";
 import Paginate from "./Paginate";
-const ShowSearch = ({ animes,setActiveAnime }) => {
+import Card from "../UI/Card";
+const ShowSearch = ({ animes,setActiveAnime,error,loading,activeAnime }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const titlePerPage = 5;
 
@@ -9,17 +10,20 @@ const ShowSearch = ({ animes,setActiveAnime }) => {
   const firstTitleIdx = lastTitleIdx - titlePerPage;
   const currentDetails = animes.slice(firstTitleIdx, lastTitleIdx);
   return (
-    <>
+    <Card header='Anime Search Results'>
       <Titles
         animeDetails={currentDetails}
         setCurrentAnime={setActiveAnime}
+        error={error}
+        loading={loading}
+        activeAnime={activeAnime}
       />
       <Paginate
         titlePerPage={titlePerPage}
         totalTitles={animes.length}
         paginate={setCurrentPage}
       />
-    </>
+    </Card>
   );
 };
 
