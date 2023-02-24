31d35c82e640a1f26816d189901c7d2d743c677c
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-02-24 13:47:29 +0530

    Conditionally rendering different result in Title.js

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,20 +1,19 @@
-import { useState } from "react";
 import styles from "./Titles.module.css";
 
-const Titles = ({ animeDetails, setCurrentAnime }) => {
-  const [activeId, setActiveId] = useState();
+const Titles = ({ animeDetails, setCurrentAnime,error,loading,activeAnime }) => {
 
   const handleAnimeClick = (e) => {
     const id = parseInt(e.target.getAttribute("data-index"));
-    setActiveId(id);
     setCurrentAnime(id);
   };
 
-  let content = <h2>No anime found! Try Again</h2>;
+  let content = <p>Search Any anime. To get Started</p>;
+  if(loading) content = <p>Loading...</p>
+  if(loading=== false && animeDetails.length === 0)//The default value for loading is null, so if nothing was searched it will display search line number 10, but if we search loading will become true and than false, so if no anime were found for that name, we will return below content
+  content = <p>No Anime found. Try Again!</p>
+  if(error) content = <p>{error}</p>
   if (animeDetails.length !== 0)
     content = (
-      <>
-      <h3>Anime Search Results</h3>
         <ul className={styles.list}>
           {animeDetails.map((detail) => (
             <li
@@ -22,14 +21,13 @@ const Titles = ({ animeDetails, setCurrentAnime }) => {
               key={detail.index}
               onClick={handleAnimeClick}
               className={`${styles.li} ${
-                detail.index === activeId ? styles.active : ""
+                detail.index === activeAnime ? styles.active : ""
               }`}
             >
               {detail.name}
             </li>
           ))}
         </ul>
-      </>
     );
   // if (loading) content = <p>Loading...</p>;
   // if (error) content = <p>{error}</p>;
